20c2aac8fcf99ecf28a68ce57fa7ce7fd6a1ebbb
Xiaojian Wang <glancky@gmail.com>
2015-10-16 12:55:17 -0700

    Initial Commit

---
+++ FrequentPatternCompressor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FrequentPatternCompressor
+//
+//  Created by Xiaojian Wang on 10/16/15.
+//  Copyright © 2015 Xiaojian Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
